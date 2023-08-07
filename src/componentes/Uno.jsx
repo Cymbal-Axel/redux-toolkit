d05d14b32c8a34c1ab61c622bf5838ac0b886af8
Axel <cymbalaxel@gmail.com>
2023-08-07 17:42:07 -0300

    se importo useDispatch y useSelector

--- src/componentes/Uno.jsx
+++ src/componentes/Uno.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Uno = () => {
+    const dispatch = useDispatch();
+    const miNombreAhora = useSelector(state => state.unValor.miNombre)
+
     return(
-        <div>Uno</div>
+        <>
+        <h1>{miNombreAhora}</h1>
+        </>
     )
 };
 
