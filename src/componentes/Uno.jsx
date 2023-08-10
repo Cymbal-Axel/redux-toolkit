830055ba1adbac2fd9f87823defb8fc40cd3ad4b
Axel <cymbalaxel@gmail.com>
2023-08-10 17:26:58 -0300

    se creo un boton para modificar el nombre con una funcion recien creada llamada "modificar" y se importo el slice "guardarMiNombre"

--- src/componentes/Uno.jsx
+++ src/componentes/Uno.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { guardarMiNombre } from '../store/misSlice';
 
 const Uno = () => {
     const dispatch = useDispatch();
     const miNombreAhora = useSelector(state => state.unValor.miNombre)
 
+    const modificar = () => {
+        dispatch(guardarMiNombre('Bob el constructor'))
+    }
     return(
         <>
         <h1>{miNombreAhora}</h1>
+        <button onClick={modificar}>Modificar Nombre</button>
         </>
     )
 };
