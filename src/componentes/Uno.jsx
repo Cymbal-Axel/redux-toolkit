25bf1e530e1c590020d999c16d2ac2f12239efa6
Axel <cymbalaxel@gmail.com>
2023-08-11 16:19:26 -0300

    se importo incrementarPuntuacion, se agrego la funciona neuva "incrementar" y se agrego un boton para incrementar con un onClick

--- src/componentes/Uno.jsx
+++ src/componentes/Uno.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { guardarMiNombre } from '../store/misSlice';
+import { guardarMiNombre, incrementarPuntuacion } from '../store/misSlice';
 
 const Uno = () => {
     const dispatch = useDispatch();
     const miNombreAhora = useSelector(state => state.unValor.miNombre)
+    const miPuntiacionAhora = useSelector(state => state.otroValor.puntuacion)
 
     const modificar = () => {
         dispatch(guardarMiNombre('Bob el constructor'))
     }
+
+    const incrementar = () => {
+        dispatch(incrementarPuntuacion())
+    }
     return(
         <>
         <h1>{miNombreAhora}</h1>
+        <h1>{miPuntiacionAhora}</h1>
+        <button onClick={incrementar}>incrementar puntuacion</button>
         <button onClick={modificar}>Modificar Nombre</button>
         </>
     )
